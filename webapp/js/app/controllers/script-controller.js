40b42628df0bf54c45c976ed5c1bdbf134bce5e6
Nancy Xie <nancyxie@Nancys-MacBook-Pro.local>
2019-04-15 13:04:28 -0500

    changes

--- webapp/js/app/controllers/script-controller.js
+++ webapp/js/app/controllers/script-controller.js
@@ -5,18 +5,30 @@ var prefix =
   "PREFIX movies:<http://www.movieontology.org/2009/10/01/movieontology.owl#>" +
   "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
   "PREFIX movie: <http://data.linkedmdb.org/resource/movie/>" +
-  "PREFIX toys: <http://www.semanticweb.org/amazon_toy_products#>";
+  "PREFIX toys: <http://www.semanticweb.org/amazon_toy_products#>" +
+  "PREFIX xds: <http://www.w3.org/2001/XMLSchema#>";
 var namedGraphs =
   "FROM NAMED <http://localhost:3030/MoviesToys/data/Movies>" +
   "FROM NAMED <http://localhost:3030/MoviesToys/data/Toys>";
 
-var temp = temp(basequery, formatquery, prefix);
 var q1 = queryone(basequery, formatquery, prefix, namedGraphs);
+//var temp = temp(basequery, formatquery, prefix);
+var PageView = this;
+PageView.currentView = ko.observable("ScenarioView");
+PageView.Home = function() {
+  PageView.currentView("ScenarioView");
+  console.log(PageView.currentView);
+};
+PageView.About = function() {
+  PageView.currentView("AboutView");
+  console.log("Hello World");
+  console.log(PageView.currentView);
+};
 
-
+ko.applyBindings(PageView);
 
 function temp(basequery, formatquery, prefix) {
-    console.log("Temp");
+  console.log("Temp");
   //console.log(hello);
   //var basequery = "http://localhost:3030/MoviesToys?query="
   //var formatquery = "&output=json"
@@ -34,7 +46,7 @@ function temp(basequery, formatquery, prefix) {
   var queryone = new XMLHttpRequest();
 
   // Open a new connection, using the GET request on the URL endpoint
-  queryone.open("GET", complete_q1, false);
+  queryone.open("GET", complete_q1);
 
   queryone.onload = function() {
     // Begin accessing JSON data here
@@ -79,7 +91,7 @@ function temp(basequery, formatquery, prefix) {
 
       var options = {
         title: "Genre per Movies",
-        chartArea: { width: "100%" , height: "100%"},
+        chartArea: { width: "100%", height: "100%" },
         hAxis: {
           title: "Genre"
         },
@@ -99,123 +111,227 @@ function temp(basequery, formatquery, prefix) {
   queryone.send();
 }
 function queryone(basequery, formatquery, prefix, namedGraphs) {
-    var length_category = 0;
-  console.log("Query One");
-    //title
-  var final_arr = [];
-  var title = ["Company", "Number of Toys Based on Production Company"];
-    final_arr.push(title);
+  // the only query needed for all our scenarios
 
-    
-// finding toys based on descriptions that have the keyword of the production companies 
-    
-// only looking at 3 genres: adventure, family, animation (this is because the dataset is too large)
-   
-  var str_q1_1 =
-    "SELECT   ?company (count(?toy_descript) as ?NumToy)" +
+  var query_1 =
+    "SELECT ?title ?prodCompany ?budget ?revenue ?rating (count(?toyName) as ?toyCount) (avg(xds:float(?toyRate)) as ?avgToyRating) (avg(xds:float(?toyCoster)) as ?avgToyCost)" +
     namedGraphs +
-    " WHERE { {GRAPH ?g {?idName toys:product_description ?toy_descript;" +
-    "toys:product_name ?toy." +
-    "{SELECT DISTINCT ?company" +
-    " WHERE {GRAPH ?g{{?s movies:production_company ?company;" +
-    "movies:orginal_title ?title;" +
-    'movies:genre "Animation".}' +
-    "UNION" +
-    "{?s movies:production_company ?company;" +
-    "movies:orginal_title ?title;" +
-    'movies:genre "Family".} ' +
-    "UNION" +
-    "{?s movies:production_company ?company;" +
-    "movies:orginal_title ?title;" +
-    'movies:genre "Adventure".}}}' +
-    "Limit 100000" +
+    " WHERE {" +
+    "Graph ?g { ?subject movies:orginal_title ?title;" +
+    " movies:production_company ?prodCompany;" +
+    " movies:budget ?budget;" +
+    " movies:revenue ?revenue;" +
+    " movies:vote_avg ?rating." +
+    "{SELECT ?toyName ?toyDescription (SUBSTR(?toyRating,0,2) as ?toyRate) (SUBSTR(?toyCost,2) as ?toyCoster)" +
+    " WHERE{" +
+    "Graph ?g {?s toys:product_name ?toyName;" +
+    " toys:description ?toyDescription;" +
+    "toys:avg_review_rating ?toyRating;" +
+    "toys:price ?toyCost.}" +
+    "  }" +
     "}" +
-    "filter contains(?toy_descript,?company)}}} group by ?company ORDER BY DESC(?NumToy)";
-    
-      var str_q1_2 =
-    "SELECT   ?company (count(?toy) as ?NumToy)" +
-    namedGraphs +
-    " WHERE { {GRAPH ?g {?idName toys:product_description ?toy_descript;" +
-    "toys:product_name ?toy." +
-    "{SELECT DISTINCT ?company" +
-    " WHERE {GRAPH ?g{{?s movies:production_company ?company;" +
-    "movies:orginal_title ?title;" +
-    'movies:genre "Animation".}' +
-    "UNION" +
-    "{?s movies:production_company ?company;" +
-    "movies:orginal_title ?title;" +
-    'movies:genre "Family".} ' +
-    "UNION" +
-    "{?s movies:production_company ?company;" +
-    "movies:orginal_title ?title;" +
-    'movies:genre "Adventure".}}}' +
-    "Limit 100000" +
     "}" +
-    "filter contains(?toy,?company)}}} group by ?company ORDER BY DESC(?NumToy)";
+    "filter ((contains(?toyName,?title) || contains(?toyDescription,?title)) && (contains(?toyName,?prodCompany) || contains(?toyDescription,?prodCompany)))" +
+    "}" +
+    "Group by ?title ?prodCompany ?budget ?revenue ?rating" +
+    " ORDER BY DESC(?toyCount)";
 
-  var complete_q1 =
-    basequery + encodeURIComponent(prefix + str_q1_1) + formatquery;
-    var complete_q2 =
-    basequery + encodeURIComponent(prefix + str_q1_2) + formatquery;
-  console.log(complete_q2);
+  var complete_query =
+    basequery + encodeURIComponent(prefix + query_1) + formatquery;
 
-  var q1_1 = new XMLHttpRequest();
-  q1_1.open("GET", complete_q1);
-  q1_1.setRequestHeader ('Content-type', 'application/x-www-form-urlencoded');
-    q1_1.setRequestHeader ("Accept", "application/sparql-results+xml");
-  q1_1.onload = function() {
-    
-   var data = JSON.parse(this.response);
+  console.log(complete_query);
+
+  var d = new Date();
+  var firstTime = d.getTime();
+
+  var query_uno = new XMLHttpRequest();
+  query_uno.open("GET", complete_query);
+  query_uno.setRequestHeader(
+    "Content-type",
+    "application/x-www-form-urlencoded"
+  );
+  query_uno.setRequestHeader("Accept", "application/sparql-results+xml");
+
+  var Toy_to_Movie_Rating = [];
+  var Toy_to_Movie_Budget = [];
+  var Toy_to_Movie_Revenue = [];
+  var Toy_to_Movie_Count = [];
+
+  query_uno.onload = function() {
+    var data = JSON.parse(this.response);
     //console.log(data);
     data = data.results.bindings;
-      //console.log(data);
+    //console.log(data);
     //console.log(data[0].valueOf());
 
-    if (q1_1.status >= 200 && q1_1.status < 400) {
-      
+    if (query_uno.status >= 200 && query_uno.status < 400) {
+      var title_Toy_to_Movie_Count = ["Movie Title", "Number of Toys"];
+      Toy_to_Movie_Count.push(title_Toy_to_Movie_Count);
+
+      Toy_to_Movie_Budget.push(
+        ["Movie Budget", "Number of Toys"],
+        ["Movie Budget < $1,000,000", 0],
+        ["$1,000,000 <= Movie Budget < $10,000,000", 0],
+        ["$10,000,000 <= Movie Budget < $50,000,000", 0],
+        ["$50,000,000 <= Movie Budget < $100,000,000", 0],
+        ["$100,000,000 <= Movie Budget < $200,000,000", 0],
+        ["$200,000,000 <= Movie Budget", 0]
+      );
+
+      Toy_to_Movie_Revenue.push(
+        ["Movie Budget", "Number of Toys"],
+        ["Movie Budget < $1,000,000", 0],
+        ["$1,000,000 <= Movie Budget < $10,000,000", 0],
+        ["$10,000,000 <= Movie Budget < $50,000,000", 0],
+        ["$50,000,000 <= Movie Budget < $100,000,000", 0],
+        ["$100,000,000 <= Movie Budget < $200,000,000", 0],
+        ["$200,000,000 <= Movie Budget", 0]
+      );
       data.forEach(type => {
+        var inner_Toy_to_Movie_Count = [];
         //console.log(final_arr)
         //console.log(type.length);
         //console.log(type.genre.value)
-        var ar = new Array(2);
-        ar[0] = type.company.value;
-        ar[1] = parseInt(type.NumToy.value);
-        final_arr.push(ar);
+
+        /* var inner_Toy_to_Movie_Rating = [];
+        var title_Toy_to_Movie_Rating = ["Movie Title", "Movie Rating "];
+        Toy_to_Movie_Rating.push(title_Toy_to_Movie_Rating);
+
+        var inner_Toy_to_Movie_Budget = [];
+        var title_Toy_to_Movie_Budget = ["Movie Title", "Movie Budget "];
+        Toy_to_Movie_Budget.push(title_Toy_to_Movie_Budget);
+
+        var inner_Toy_to_Movie_Revenue = [];
+        var title_Toy_to_Movie_Revenue = ["Movie Title", "Movie Budget "];
           
+        var inner_Toy_to_Movie_Rating = [];
+        var title_Toy_to_Movie_Rating = ["Movie Title", "Movie Rating "]; */
+        /* inner_Toy_to_Movie_Rating[0] = type.title.value;
+        inner_Toy_to_Movie_Budget[0] = type.title.value;
+        //inner_Toy_to_Movie_Revenue[0] = type.title.value; */
+        inner_Toy_to_Movie_Count[0] = type.title.value;
+
+        /* inner_Toy_to_Movie_Rating[1] = type.title.value;
+        inner_Toy_to_Movie_Budget[1] = type.budget.value;
+        inner_Toy_to_Movie_Revenue[1] = type.title.value; */
+        inner_Toy_to_Movie_Count[1] = parseInt(type.toyCount.value);
+
+        /* ar[1] = parseInt(type.prodCompany.value);
+        ar[2] = parseInt(type.budget.value);
+        ar[3] = parseInt(type.revenue.value);
+        ar[4] = parseFloat(type.rating.value);
+        ar[5] = parseInt(type.toyCount.value);
+        ar[6] = parseFloat(type.avgToyRating.value);
+        ar[7] = parseFloat(type.avgToyCost.value); */
+        //console.log(type.budget.value);
+        Toy_to_Movie_Budget = ret_count_arr(
+          Toy_to_Movie_Budget,
+          parseInt(type.budget.value)
+        );
+
+        Toy_to_Movie_Revenue = ret_count_arr(
+          Toy_to_Movie_Revenue,
+          parseInt(type.revenue.value)
+        );
+
+        Toy_to_Movie_Count.push(inner_Toy_to_Movie_Count);
       });
-        //console.log(final_arr);
-        
     } else {
       console.log("error");
     }
-    
-      return final_arr;
-      
-  };
-   // final_arr = q1_1.onload;
-   console.log(final_arr);
 
-  q1_1.send();
-    
-    
-    
-    
-   console.log(final_arr);
-    
-    
-  
-    
-    
-    
-    
+    var ToyToMovieCountDraw = draw_Bar(
+      Toy_to_Movie_Count,
+      "Number of Toys per Movie",
+      "Movies",
+      "Number of Toys",
+      "toy_movie_count"
+    );
+    console.log("Drawing PIE things");
+    var MovieBudgetPie = draw_Pie(
+      Toy_to_Movie_Budget,
+      "Movie Budget to The Amount of Toys Created",
+      "piechart_1"
+    );
+
+    var MovieRevenuePie = draw_Pie(
+      Toy_to_Movie_Budget,
+      "Movie Reven to The Amount of Toys Created",
+      "piechart_2"
+    );
+
+    console.log(Toy_to_Movie_Budget);
+  };
+
+  query_uno.send();
 }
-function findarray(arr,length_category){
-    var tmp_arr = [];
-    console.log("Inside find")
-    console.log(arr[0])
-    console.log(arr.length);
-    for( i= 0; i < arr.length; i++){
-        
-    }
-    
+function findarray(arr, length_category) {
+  var tmp_arr = [];
+  console.log("Inside find");
+  console.log(arr[0]);
+  console.log(arr.length);
+  for (i = 0; i < arr.length; i++) {}
+}
+
+function draw_Bar(arr, mainTitle, hTitle, vTitle, elementID) {
+  google.charts.load("current", { packages: ["corechart"] });
+  google.charts.setOnLoadCallback(drawStuff);
+
+  function drawStuff() {
+    //console.log(arr);
+
+    var data = google.visualization.arrayToDataTable(arr);
+
+    var options = {
+      title: mainTitle,
+      chartArea: { width: "10000px ", height: "3000px" },
+      hAxis: {
+        title: hTitle
+      },
+      vAxis: {
+        title: vTitle
+      }
+    };
+
+    var chart = new google.visualization.ColumnChart(
+      document.getElementById(elementID)
+    );
+    chart.draw(data, options);
+  }
+}
+function draw_Pie(arr, mainTitle, elementID) {
+  console.log("Entered Pie Chart");
+  google.charts.load("current", { packages: ["corechart"] });
+  google.charts.setOnLoadCallback(drawPieChart);
+
+  function drawPieChart() {
+    var Pie_data = google.visualization.arrayToDataTable(arr);
+
+    var Pie_options = {
+      title: mainTitle
+    };
+
+    var chart = new google.visualization.PieChart(
+      document.getElementById(elementID)
+    );
+
+    chart.draw(Pie_data, Pie_options);
+  }
+}
+function ret_count_arr(count_arr, budget) {
+  if (budget < 1000000) {
+    count_arr[1][1]++;
+  } else if (1000000 <= budget && budget < 10000000) {
+    count_arr[2][1]++;
+  } else if (10000000 <= budget && budget < 50000000) {
+    count_arr[3][1]++;
+  } else if (50000000 <= budget && budget < 100000000) {
+    count_arr[4][1]++;
+  } else if (100000000 <= budget && budget < 250000000) {
+    count_arr[5][1]++;
+  } else {
+    count_arr[6][1]++;
+  }
+
+  return count_arr;
 }
